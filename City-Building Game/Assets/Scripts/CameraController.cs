6963e542f0f86a6262bc147743f82d8b47f6663a
Aaron Hamilton <aaronleon28@gmail.com>
2021-04-17 15:31:22 +0100

    Initializing the camera
    We had created an object to be our camera anchor ands linked it to the controller script. Within the script we had initialised the camera and linked the cameras variables.
    Project: Nyala

--- City-Building Game/Assets/Scripts/CameraController.cs
+++ City-Building Game/Assets/Scripts/CameraController.cs
@@ -4,10 +4,37 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    #region Variables
+    //The movement speed of the camera and zoom and rotation.
+    public float movementSpeed;
+    public float zoomSpeed;
+    public float rotapeed;
+
+    //Setting the rotation limits.
+    public float minRotate;
+    public float MaxRotate;
+
+
+    //Setting the max zoom in and out.
+    public float minZoom;
+    public float maxZoom;
+
+    //The cameras current rotation and zoom.
+    public float camCurRot;
+    public float camCurZoom;
+
+    //Conecting the camera.
+    private Camera cameraX;
+    #endregion
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Linking the camera to the main and setting the defaults.
+        cameraX = Camera.main;
+        camCurZoom = cameraX.transform.localPosition.y;
+        camCurRot = -50;
     }
 
     // Update is called once per frame
